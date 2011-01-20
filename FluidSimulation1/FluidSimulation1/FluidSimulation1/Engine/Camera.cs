3f7821f86d7b8577d31bc14fa73e321fc4f3a6e2
Martin Persson <marpe357@student.liu.se>
2011-01-20 18:51:44 +0100

    Lagt till n?gra klasser

---
+++ FluidSimulation1/FluidSimulation1/FluidSimulation1/Engine/Camera.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace FluidSimulation1
+{
+    public class Camera
+    {
+        public Vector3 Position = Vector3.Zero;
+        public float Fov = 90;
+
+        public Matrix Projection;
+        public Matrix View;
+        public Matrix Rotation = Matrix.Identity;
+
+        public float AspectRatio = 4 / 3;
+        public float NearPlane = 0.01f;
+        public float FarPlane = 100f;
+
+        public Camera()
+        {
+
+        }
+
+        private void BuildProjectionMatrix()
+        {
+            this.Projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(Fov), AspectRatio, NearPlane, FarPlane);
+        }
+
+        public void BuildViewMatrix()
+        {
+            Rotation.Translation = Position;
+            View = Matrix.Invert(Rotation);
+        }
+
+        public void Update()
+        {
+            //HandleInput();
+            BuildViewMatrix();
+            BuildProjectionMatrix();
+        }
+    }
+}
---
+++ FluidSimulation1/FluidSimulation1/FluidSimulation1/Sim/FluidParticle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace FluidSimulation1
+{
+    public class FluidParticle
+    {
+        public int Mass;
+        public float Density;
+
+        public Vector3 Force;
+        public Vector3 Velocity;
+        public Vector3 Position;
+
+        public TimeSpan LifeSpan;
+        public float Pressure;
+    }
+}
